ddfcca696dbc466446a94a3fb250d60cb0456c9f
Francesco Patanè <43784386+Francesco-Patane@users.noreply.github.com>
2018-10-15 18:43:00 +0200

    Add files via upload

---
+++ build/precache-manifest.e751cc8be11210b45a50d3d9fb9f86c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3b3c57a529b728ead928",
+    "url": "/static/js/main.3b3c57a5.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "3b3c57a529b728ead928",
+    "url": "/static/css/main.b53fe5ea.chunk.css"
+  },
+  {
+    "revision": "d7dbe3743febf320c51c988c33f88cd2",
+    "url": "/index.html"
+  }
+];
